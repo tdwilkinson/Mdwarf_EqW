0875c3be5aea225621c50ea239349ef5ccd6b056
Tessa D. Wilkinson <tdwilkinson@users.noreply.github.com>
2016-02-26 01:44:20 -0800

    outputs one kicnumber and one ha and error

--- Mdwarf_EqW.py
+++ Mdwarf_EqW.py
@@ -1,6 +1,7 @@
 __author__ = 'Tessa Wilkinson'
 """
- run in python console
+- run in python console
+- change the path at the bottom of the line to search a directory of UT directories for images.
  outfile: finalEW.txt
 """
 import os
@@ -12,13 +13,14 @@ from scipy.interpolate import UnivariateSpline as spline
 import pandas as pd
 from numpy import trapz  # trapezoidal sum of the area
 
-# global variables = which lines to measure and their ranges.
+# global variables = which lines to measure
 halpha = 6562.801
 tio5 = 0
 
-def setrange(line):
-    lower, upper = [line - 5, line + 5]
-    return lower, upper
+# set up output file and header
+newfile = open('finalEW.txt', 'w')
+newfile.write('kicnumber' + '\t' + 'ha_ew' + '\t' + 'ha_ew_err' + '\n')
+
 
 def get_wavelength_calibrated_fits_files(input_directory):
     """
@@ -26,73 +28,73 @@ def get_wavelength_calibrated_fits_files(input_directory):
     and look for the wavelength calibrated fits images and save their path locations to a list.
     output = list of paths to fits files
     """
-    # fits_files = []
-    # for file in os.listdir(input_directory):
-    #     if file.startswith('dkic') and file.endswith('.fits'):
-    #         fits_files.append(file)
-    # return fits_files
 
     check = []
     fits_path = []
-    for root, dirs, files in os.walk(highpath, topdown=False):
+    for root, dirs, files in os.walk(input_directory, topdown=False):
         for name in files:
             if (os.path.join(root, name)) not in check and name.startswith('dkic') and name.endswith('.fits'):
-                # print(os.path.join(root,name)) # to get whole path to fits file
                 check.append(os.path.join(root, name))
-
                 fits_path.append(os.path.join(root, name))
 
     return fits_path
 
-
 def find_halpha(r, data, plot):
     """
+     define halpha
     :param r:
     :param data: the flux values for the data
     :param plot: if you want to plot these values
     :return:variables at the moment
     """
-    # define halpha
 
-    ind1 = {}
-    ind2 = {}
     ha_region = {}
+    lower, upper = setrange(halpha)
+
+    for i, j in zip(r, data):
+        if i >= lower and i <= upper:
+            ha_region[i] = j
+
+    return ha_region
+
+def find_continuum(r, data, flux):
+    """
+    Define region just outside of halpha to be continuum
+    Currently set to 10 angstroms wide
+    :return:
+    """
+    # TODO: set ha check to get max or min value in range
+    # TODO: then set continuum to be much closer to actual ha dip/peak
+
+    cont_range_preline = {}
+    cont_range_postline = {}
+    preline_nocr = {}
+    postline_nocr = {}
+
+
     lower, upper = setrange(halpha)
     for i, j in zip(r, data):
         if i >= lower-10 and i <= lower:
-            ind1[i] = j
-            if plot:
-                plt.axvline(x=i, linestyle='--', color='pink')
-
+            cont_range_preline[i] = j
         elif i >= upper and i <= upper+10:
-            ind2[i] = j
-            if plot:
-                plt.axvline(x=i, linestyle='--', color='pink')
+            cont_range_postline[i] = j
 
-        elif i >= lower and i <= upper:
-            ha_region[i] = j
+    # remove cosmic rays from the continuum regions by setting limits
+    for n, (w, flux) in enumerate(cont_range_preline.iteritems()):
+        if cont_range_preline.values()[n - 1] < flux * 1.25 and cont_range_postline.values()[n - 1] > flux * 0.75:
+            preline_nocr[w] = flux
+    for n, (w, flux) in enumerate(cont_range_postline.iteritems()):
+        if cont_range_postline.values()[n - 1] < flux * 1.25 and cont_range_postline.values()[n - 1] > flux * 0.75:
+            postline_nocr[w] = flux
 
-    return ha_region, ind1, ind2
-
-def find_continuum(ind1, ind2, flux):
-    # remove cosmic rays from the continuum regions by setting
-    ind1_nocr = {}
-    ind2_nocr = {}
-    for n, (w, flux) in enumerate(ind1.iteritems()):
-        if ind1.values()[n - 1] < flux * 1.25 and ind1.values()[n - 1] > flux * 0.75:
-            ind1_nocr[w] = flux
-    for n, (w, flux) in enumerate(ind2.iteritems()):
-        if ind2.values()[n - 1] < flux * 1.25 and ind2.values()[n - 1] > flux * 0.75:
-            ind2_nocr[w] = flux
-
-        # define continuum
-    c1 = np.mean(ind1_nocr.values())
-    c2 = np.mean(ind2_nocr.values())
+    # define continuum
+    c1 = np.mean(preline_nocr.values())
+    c2 = np.mean(postline_nocr.values())
     continuum = np.mean([c1, c2])
     contupper = continuum * 1.25  # limits for graphing
     contlower = continuum * .75  # limits for graphing
 
-    return continuum, contupper, contlower, c1, c2, ind2_nocr
+    return continuum, contupper, contlower, c1, c2, postline_nocr
 
 def measure_ew(ha_region, continuum, c1, c2, ind2_nocr):
     # measure area under feature
@@ -113,26 +115,32 @@ def measure_ew(ha_region, continuum, c1, c2, ind2_nocr):
     return ew, area
 
 def save_ew(fitsimage, ew):
+    """
+    output: text file with tab separated values of each kic number followed by EqW measurements
+    """
+
     ew_dict = {}
-    # single out kic to compare with prot kic
+
     fi = fitsimage.find('kic')
     tmp = fitsimage[fi + 3:]
     dot = tmp.find('.')
     kic = int(tmp[:dot])  # gets just the kic number
 
     if kic not in ew_dict.keys():
-        ew_dict[kic] = []
-    ew_dict[kic].append(ew)
+        ew_dict[kic] = ew
 
-    newfile = open('finalEW.txt', 'w')
-    newfile.write('kicnumber' + '\t' + 'ha_ew' + '\t' + 'ha_ew_err' + '\n')
-    for k, v in sorted(ew_dict.iteritems()):
-        newfile.write(str(k) + '\t' + str(v[0][0]) + '\t' + str(v[0][1]) + '\n')
-    newfile.close()
+    # TODO: this vstack works, just need to apply weighted-average? to print best to finalEW.txt
+    # else:
+    #     stack = np.vstack((ew))
+    #     ew_dict[kic] = stack
 
-def plot_ew(halpha, contupper, contlower, continuum, area, r, data):
 
-    figure = plt.figure()
+    for k, v in sorted(ew_dict.items()):
+        newfile.write(str(k) + '\t' + str(v[0]) + '\t' + str(v[1]) + '\n')
+
+
+def plot_ew(halpha, contupper, contlower, continuum, area, r, data,):
+
     # plot halpha
     plt.axvline(x=halpha, color='g')
     # plot boundries of halpha measurements
@@ -142,17 +150,24 @@ def plot_ew(halpha, contupper, contlower, continuum, area, r, data):
     plt.axhline(contupper, color='orange')  # go to get_ew2.py
     plt.axhline(contlower, color='orange')
     plt.axhline(continuum, color='red')
+    lower, upper = setrange(halpha)
+    for i, j in zip(r, data):
+        if i >= lower-10 and i <= lower:
+            plt.axvline(x=i, linestyle='--', color='pink')
+
+        elif i >= upper and i <= upper+10:
+            plt.axvline(x=i, linestyle='--', color='pink')
+
     #plt.fill_between(halpha, continuum)
     #plt.plot(area, color = 'red' )
     plt.plot(r, data, color='black', linestyle='-', marker=',')
     plt.xlim(6530, 6600)
 
+    plt.show()
 
-    #figure.show()
 
-# TODO: currently pops up all ~80, may want to fix that before turning plot on! :)
 
-def ew_per_image(fitsimage, checkmode = False, plot = True):
+def ew_per_image(fitsimage, checkmode = False, plot = False):
     """
      :return: output file with ew's
     """
@@ -167,31 +182,19 @@ def ew_per_image(fitsimage, checkmode = False, plot = True):
     r = flux * dw + minw
 
     # define region to investigate
-    ha_region, ind1, ind2 = find_halpha(r, data, plot)
-
+    ha_region= find_halpha(r, data, plot)
     # clean investigation region of cosmic rays and define continuum region
-    continuum, contupper, contlower, c1, c2, ind2_nocr = find_continuum(ind1,ind2, flux)
-
+    continuum, contupper, contlower, c1, c2, ind2_nocr = find_continuum(r, data, flux)
     # take the area under the curve
     ew, area = measure_ew(ha_region, continuum, c1, c2, ind2_nocr)
 
     save_ew(fitsimage, ew)
 
-
-
     if plot:
         plot_ew(halpha, contupper, contlower, continuum, area, r, data)
 
-    # print ew_dict
-    # if checkmode:
-    #     print 'kicnumber:', kic
-    #     print 'continuum mean:', continuum
-    #     print 'area:', area
-    #     print 'fluxerr:', fluxerr
-    #     print 'ew:', ew
 
-
-def ew_per_directory(parent_directory, output_directory):
+def ew_per_directory(parent_directory):
     """
     output: a text file in the output_directory for each .fits file starting with dkic
             found in the parent directory
@@ -202,16 +205,32 @@ def ew_per_directory(parent_directory, output_directory):
         ew_per_image(path)
 
 
-# TODO: plot only one at a time
-# TODO: correct values printing to finalEW.txt
-# TODO: make sure multiple files for one star or stored or something
+def setrange(line):
+    # for measuring continuum
+    lower, upper = [line - 5, line + 5]
+    return lower, upper
+
+def final_plot():
+    """
+
+    :return: plot output values of ew per star
+    """
+
+    kicnumber, ha, ha_err = np.loadtxt('finalEW.txt', dtype = (int, float, float), skiprows = 1, unpack = True )
+
+    print kicnumber, ha ,ha_err
+########################################################################################################################
+########################################################################################################################
+
+
+# TODO: make sure multiple files for one star or stored or appended in list (see todo above)
 # TODO: separate ones with magnetic activity
 # TODO: add in looking at other bands
+# TODO: plot output values (maybe in new file)
 
+# 1) create output file finalEW.txt
 
+# change this line to search a directory that contains directories or files.
+ew_per_directory('/home/tessa/astronomy/mdwarf/highres_data')
+newfile.close()
 
-directory = (os.listdir('/home/tessa/astronomy/mdwarf/'))
-highpath = '/home/tessa/astronomy/mdwarf/highres_data'
-lowpath = '/home/tessa/astronomy/mdwarf/lowres_data'
-
-ew_per_directory(highpath, os.path.join(highpath, 'disp_spectra_data'))
